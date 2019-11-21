abaa6403db1fb283ac9876359b6d428cc3ff1eba
mppatterson84 <mppatterson84@gmail.com>
2019-11-20 18:11:17 -0600

    Add pages app

---
+++ pages/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        context['home_active'] = 'active'
+        context['home_active_link'] = '#'
+        context['home_active_sr'] = '<span class="sr-only">(current)</span>'
+        return context
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'About'
+        context['about_active'] = 'active'
+        context['about_active_link'] = '#'
+        context['about_active_sr'] = '<span class="sr-only">(current)</span>'
+        return context
