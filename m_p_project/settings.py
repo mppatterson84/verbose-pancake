603cf549f1bce2c993348e6cc674ed68bbb8ec3c
mppatterson84 <mppatterson84@gmail.com>
2019-12-06 10:33:09 -0600

    Upgrade to Django 3

--- m_p_project/settings.py
+++ m_p_project/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'm_p_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
