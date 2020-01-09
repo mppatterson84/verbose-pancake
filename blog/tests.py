c16c516ade688dd0c9163857ec7f9e70f258faf5
mppatterson84 <mppatterson84@gmail.com>
2020-01-08 19:00:29 -0600

    Update Tests
    Add tests to sendemail app

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from datetime import datetime
 from .models import Post
 
 class BlogTests(TestCase):
@@ -19,28 +18,20 @@ class BlogTests(TestCase):
             title='Blog Title',
             body='Body content...',
             author=testuser1,
-            created_at=datetime.date(1984, 6, 13),
-            updated_at=datetime.date(2020, 1, 22),
-            published=True,
-            categories='Test Category'
+            published=True
         )
         test_post.save()
 
-        def test_blog_content(self):
-            post = Post.objects.get(id=1)
-            expected_title = f'{post.title}'
-            expected_body = f'{post.body}'
-            expected_author = f'{post.author}'
-            expected_created_at = f'{post.created_at}'
-            expected_updated_at = f'{post.updated_at}'
-            expected_slug = f'{post.slug}'
-            expected_published = f'{True}'
-            expected_category = f'{post.categories}'
-            self.assertEqual(expected_title, 'Blog Title')
-            self.assertEqual(expected_body, 'Body content...')
-            self.assertEqual(expected_author, 'testuser1')
-            self.assertEqual(expected_created_at, datetime.date(1984, 6, 13))
-            self.assertEqual(expected_updated_at, datetime.date(2020, 1, 22))
-            self.assertEqual(expected_slug, 'blog-title')
-            self.assertEqual(expected_published, True)
-            self.assertEqual(expected_category, 'Test Category')
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        expected_title = f'{post.title}'
+        expected_body = f'{post.body}'
+        expected_author = f'{post.author}'
+        expected_slug = f'{post.slug}'
+        expected_published = f'{post.published}'
+        expected_category = f'{post.categories}'
+        self.assertEqual(expected_title, 'Blog Title')
+        self.assertEqual(expected_body, 'Body content...')
+        self.assertEqual(expected_author, 'testuser1')
+        self.assertEqual(expected_slug, 'blog-title')
+        self.assertEqual(expected_published, 'True')
--- sendemail/tests.py
+++ sendemail/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Email
 
-# Create your tests here.
+class EmailTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create an email
+        test_email = Email.objects.create(
+            from_email='test@email.com',
+            subject='Hello Test',
+            message='Email content...'
+        )
+        test_email.save()
+
+    def test_email_content(self):
+        email = Email.objects.get(id=1)
+        expected_from_email = f'{email.from_email}'
+        expected_subject = f'{email.subject}'
+        expected_message = f'{email.message}'
+        self.assertEqual(expected_from_email, 'test@email.com')
+        self.assertEqual(expected_subject, 'Hello Test')
+        self.assertEqual(expected_message, 'Email content...')
