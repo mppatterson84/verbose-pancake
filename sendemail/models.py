ad719a5ac7da870963f9ad351ecbf670197506b5
mppatterson84 <mppatterson84@gmail.com>
2020-01-15 13:18:11 -0600

    Update sendemail
    Add created at field for the email model

--- sendemail/models.py
+++ sendemail/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 class Email(models.Model):
     from_email = models.EmailField(max_length=70, blank=False)
     subject = models.CharField(max_length=70, blank=False)
     message = models.TextField(max_length=700, blank=False)
+    created_at = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return self.subject
