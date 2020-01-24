bf670a21e4039643e3c4de7e88fdda43167d4756
mppatterson84 <mppatterson84@gmail.com>
2020-01-24 12:13:49 -0600

    Update views.py
    Update sendemail address

--- sendemail/views.py
+++ sendemail/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .forms import EmailForm
+import os
 
 def email_view(request):
     if request.method == 'GET':
@@ -15,7 +16,7 @@ def email_view(request):
             message = form.cleaned_data['message']
             form.save()
             try:
-                send_mail(subject, message, from_email, ['mppatterson84@gmail.com'])
+                send_mail(subject, message, from_email, [os.environ['SEND_EMAIL_ADDRESS']])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
