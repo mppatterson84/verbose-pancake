14d07a50662dc3d4fe29f5108ff073c86c01d6b1
mppatterson84 <mppatterson84@gmail.com>
2020-01-24 12:06:43 -0600

    Update sendemail form

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -2,11 +2,22 @@ from django import forms
 from .models import Email
 
 class EmailForm(forms.ModelForm):
+    from_email = forms.CharField(
+        label='Your Email Address:', 
+        required=True, 
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "example@email.com", 
+                "class": "form-control"
+            }
+        )
+    )
+
     class Meta:
         model = Email
         fields = ["from_email", "subject", "message"]
         widgets = {
-            'from_email': forms.TextInput(attrs={"placeholder": "example@email.com", "class": "form-control"}),
+            # 'from_email': forms.TextInput(attrs={"placeholder": "example@email.com", "class": "form-control"}),
             'subject': forms.TextInput(attrs={"placeholder": "Email Subject", "class": "form-control"}),
             'message': forms.Textarea(attrs={"placeholder": "Message: 700 characters max.", "class": "form-control"}),
         }
