895f25caea2831db94947dede127b27b9a46a8c2
mppatterson84 <mppatterson84@gmail.com>
2020-01-27 23:15:26 -0600

    Update home page

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,28 @@
 console.log('Hello, World!');
+
+const progress = document.querySelectorAll("div.home-container");
+
+function pageValues() {
+    var cHeight = document.body.clientHeight;
+    var wHeight = window.innerHeight;
+    var scrollOffset = window.pageYOffset;
+    var onePercentOfHeight = (cHeight - wHeight) / 100;
+    var scrollPosition = scrollOffset / onePercentOfHeight;
+
+    return {
+        "scrollPosition": scrollPosition,
+        "cHeight": cHeight,
+        "wHeight": wHeight,
+        "scrollOffset": scrollOffset
+    }
+}
+
+window.addEventListener("load", function () {
+    progress[0].style.backgroundPositionY = `-${pageValues().scrollPosition * 3}px`;
+    progress[1].style.backgroundPositionY = `-${pageValues().scrollPosition * 3 + 500}px`;
+});
+
+window.addEventListener("scroll", function () {
+    progress[0].style.backgroundPositionY = `-${pageValues().scrollPosition * 3}px`;
+    progress[1].style.backgroundPositionY = `-${pageValues().scrollPosition * 3 + 500}px`;
+});
