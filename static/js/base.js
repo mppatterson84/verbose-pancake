9be9ee7abb4a62a788e29635567564ed04f0789e
mppatterson84 <mppatterson84@gmail.com>
2020-02-07 17:26:33 -0600

    Add animated hamburger

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,22 @@
 console.log("Hello, World! base.js");
+
+var hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    hamburger.classList.toggle("rubberBand");
+    hamburger.classList.toggle("jello");
+    doubleClickDelay();
+});
+
+//disables the hamburger button one millasecond after it is clicked
+//and remains disabled for half a second preventing double clicking by the user
+function doubleClickDelay() {
+    setTimeout(() => {
+        hamburger.disabled = true;
+        hamburger.attributes
+        setTimeout(() => {
+            hamburger.disabled = false;
+        }, 500);
+    }, 1);
+}
