d4d9bbf56d323ceca5fa013086c96a6fa4a4e8b8
mppatterson84 <mppatterson84@gmail.com>
2020-04-18 21:28:44 -0500

    Update base.py

--- m_p_project/settings/base.py
+++ m_p_project/settings/base.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
 
     # 3rd Party
     'ckeditor',
