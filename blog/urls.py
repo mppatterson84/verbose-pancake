b9880489bcc6ab1a5901ced16898f4321e530121
mppatterson84 <mppatterson84@gmail.com>
2020-04-20 09:18:06 -0500

    Update blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
-    PostListView, 
-    PostDetailView, 
-    PostCreateView, 
-    PostUpdateView, 
-    PostDeleteView, 
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
     PostListAllView,
     PostSearchView,
     CategoryListView,
@@ -15,14 +15,16 @@ from .views import (
 
 urlpatterns = [
     path('blog/', PostListView.as_view(), name='blog'),
-    path('blog/<int:pk>/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('blog/new/', PostCreateView.as_view(), name='post-new'),
-    path('blog/<int:pk>/<slug:slug>/edit/', PostUpdateView.as_view(), name='post-edit'),
-    path('blog/<int:pk>/<slug:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('blog/<slug:slug>/edit/', PostUpdateView.as_view(), name='post-edit'),
+    path('blog/<slug:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('blog/search/', PostSearchView.as_view(), name='blog-search'),
     path('blog/all/', PostListAllView.as_view(), name='post-list-all'),
     path('blog/categories/', CategoryListView.as_view(), name='category-list'),
     path('blog/categories/new/', CategoryCreateView.as_view(), name='category-new'),
-    path('blog/categories/<slug:slug>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
-    path('blog/categories/filter/', CategoryFilterView.as_view(), name='category-filter'),
+    path('blog/categories/<slug:slug>/delete/',
+         CategoryDeleteView.as_view(), name='category-delete'),
+    path('blog/categories/filter/',
+         CategoryFilterView.as_view(), name='category-filter'),
+    path('blog/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
