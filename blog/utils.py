757afd504f1564fe357447f4d589f868adea8fff
mppatterson84 <mppatterson84@gmail.com>
2020-04-26 07:35:50 -0500

    Update utils.py
    Refactor slug_logic function

--- blog/utils.py
+++ blog/utils.py
@@ -5,13 +5,13 @@ def slug_logic(self, i):
     from blog.models import Post
     i += 1
     slug_filter = Post.objects.filter(slug__exact=self.slug)
-    if len(slug_filter) > 0:
+    if len(slug_filter) == 1:
         if not slug_filter[0].pk == self.pk:
-            self.slug = f"{slugify(self.title)}-{len(slug_filter)+i}"
+            self.slug = f"{slugify(self.title)}-{i}"
             slug_logic(self, i)
 
 
 def get_unique_slug(self):
     self.slug = slugify(self.title)
-    i = 0
+    i = 1
     slug_logic(self, i)
