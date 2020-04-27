8cea8c86d34a6c9396022f208cf44770fb971024
mppatterson84 <mppatterson84@gmail.com>
2020-04-26 19:47:31 -0500

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,6 @@
-console.log("Hello, World! home.js");
+console.log('Hello, World! home.js');
 
-const section = document.querySelectorAll("div.home-container");
+const section = document.querySelectorAll('div.home-container');
 
 function scrolled() {
     var cHeight = document.body.clientHeight;
@@ -9,32 +9,44 @@ function scrolled() {
     var onePercentOfHeight = (cHeight - wHeight) / 100;
     var scrollPosition = scrollOffset / onePercentOfHeight;
 
-    return {
-        "scrollPosition": scrollPosition,
-        "cHeight": cHeight,
-        "wHeight": wHeight,
-        "scrollOffset": scrollOffset
-    }
+    // return {
+    //     "scrollPosition": scrollPosition,
+    //     "cHeight": cHeight,
+    //     "wHeight": wHeight,
+    //     "scrollOffset": scrollOffset
+    // }
 }
 
-window.addEventListener("load", function () {
-    section[0].style.backgroundPositionY = `-${scrolled().scrollPosition * 3}px`;
-    section[1].style.backgroundPositionY = `${scrolled().scrollPosition * 3 - 200}px`;
-    section[2].style.backgroundPositionY = `-${scrolled().scrollPosition * 3 - 150}px`;
+window.addEventListener('load', function () {
+    section[0].style.backgroundPositionY = `-${
+        scrolled().scrollPosition * 3
+    }px`;
+    section[1].style.backgroundPositionY = `${
+        scrolled().scrollPosition * 3 - 200
+    }px`;
+    section[2].style.backgroundPositionY = `-${
+        scrolled().scrollPosition * 3 - 150
+    }px`;
 
     function scrollTo(hash) {
-        location.hash = "#" + hash;
+        location.hash = '#' + hash;
     }
-    scrollTo("home");
+    scrollTo('home');
 });
 
-window.addEventListener("scroll", function () {
-    section[0].style.backgroundPositionY = `-${scrolled().scrollPosition * 3}px`;
-    section[1].style.backgroundPositionY = `${scrolled().scrollPosition * 3 - 200}px`;
-    section[2].style.backgroundPositionY = `-${scrolled().scrollPosition * 3 - 150}px`;
+window.addEventListener('scroll', function () {
+    section[0].style.backgroundPositionY = `-${
+        scrolled().scrollPosition * 3
+    }px`;
+    section[1].style.backgroundPositionY = `${
+        scrolled().scrollPosition * 3 - 200
+    }px`;
+    section[2].style.backgroundPositionY = `-${
+        scrolled().scrollPosition * 3 - 150
+    }px`;
 });
 
 // zenscroll settings
-var defaultDuration = 2000 // ms
-var edgeOffset = 0 // px
-zenscroll.setup(defaultDuration, edgeOffset)
+var defaultDuration = 2000; // ms
+var edgeOffset = 0; // px
+zenscroll.setup(defaultDuration, edgeOffset);
